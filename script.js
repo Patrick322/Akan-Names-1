9f7e7832b19063ef8500bd90f968411b0c0c724b
SaintieMonte10 <claremonte53@gmai.com>
2020-02-15 21:12:04 +0300

    added javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+var submit = document.getElementById("submit");
+
+submit.addEventListener("click", function(){
+    var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+    var dd = document.getElementById("day").value;
+    var mm = document.getElementById("month").value;
+    var yy = document.getElementById("year").value;
+    var date = new Date(yy + "-" + mm + "-" + dd);
+    var daysOfWeek = date.getDay();
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    var output = document.getElementById("output");
+    var name = document.getElementById("name").value;
+
+    if (dd < 1 || dd > 31){
+        alert("Please input a valid day!");
+    }
+    else if(mm < 1 || mm > 12){
+        alert("please input a valid month!");
+    }
+    
+    else if(yy.toString().length !== 4) {
+        alert("please input avalid year!");
+    }
+    else if(male.checked){
+        output.innerHTML = "Hurray!" + name + " .Your Akan Name Is" + maleAkanNames[daysOfWeek]; 
+    }else{
+        output.innerHTML = "Hurray!" + name + " .Your Akan Name Is" + femaleAkanNames[daysOfWeek];
+    }
+})
