b2505627f0f8170008ff153bcac4fba2d8423334
SaintieMonte10 <claremonte53@gmai.com>
2020-02-16 20:28:22 +0300

    added a description on read me

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Akan Names
+
+## Description
+
+### Akan Name comes from a traditional naming system in Ghana. A child's name is determined by the factors sorounding his or     her birth.For example,the day of their birth or in the manner which they are born may determine which namethey get           irrespective of their gender.
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var submit = document.getElementById("submit");
 
 submit.addEventListener("click", function() {
+    var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
     var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
     var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-    var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 
     var dd = document.getElementById("day").value;
